4f1b5d2fe115dabbc2d16e2932e20fd0f0f962a3
franky3er <frankdreyer94@gmail.com>
2017-04-25 16:12:48 +0200

    master - initial commit

---
+++ src/vs/fridgewebserver/main/MainApplication.java
@@ -0,0 +1,11 @@
+package vs.fridgewebserver.main;
+
+/**
+ * Created by franky3er on 25.04.17.
+ */
+public class MainApplication {
+
+    public static void main(String []args){
+        
+    }
+}
