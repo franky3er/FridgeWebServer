ac266007eb60fac8bd295356e23b7725eb846409
franky3er <frankdreyer94@gmail.com>
2017-04-28 07:17:46 +0200

    feature/config - create config key attributes

--- src/vs/fridgewebserver/main/MainApplication.java
+++ src/vs/fridgewebserver/main/MainApplication.java
@@ -5,6 +5,7 @@ import vs.fridgewebserver.http.HTTPServer;
 import vs.products.iohandler.ProductIOHandler;
 import vs.products.iohandler.database.sqlite.ProductSQLiteHandler;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -14,6 +15,15 @@ import java.util.concurrent.BlockingQueue;
 
 
 public class MainApplication {
+    private final static String PROJECT_NAME = "FridgeWebServer";
+    private final static String PROJECT_CONFIG = System.getProperty("user.dir") + File.separator +
+            "config" + File.separator + PROJECT_NAME + ".properties";
+
+    private final static String FRIDGEWEBSERVER_PRODUCTSQLITE_FILESOURCE = "FridgeWebServer.ProductSQLiteHandler.FileSource";
+    private final static String FRIDGEWEBSERVER_PRODUCTSQLITE_DRIVER = "FridgeWebServer.ProductSQLiteHandler.Driver";
+    private final static String FRIDGEWEBSERVER_WORKERS_NUMBEROF = "FridgeWebServer.Workers.NumberOf";
+    private final static String FRIDGEWEBSERVER_LISTENING_PORT = "FridgeWebServer.Listening.Porrt";
+
     private static List<HTTPClientHandler> workers;
     private static HTTPServer boss;
     private static BlockingQueue<Socket> clients = new ArrayBlockingQueue<Socket>(1024);
